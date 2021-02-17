ddab16d949cea15d39105161c465071fe3ea32da
pietro <pietro5801@gmail.com>
2021-02-17 13:41:59 +0100

    .

--- client.py
+++ client.py
@@ -1,51 +1,47 @@
 import socket
 
-# TEST SU COME FUNZIONA LA COMUNICAZIONE TRA CLIENT E SERVER
-# input_string = 'Hello'
-# print(type(input_string))
-# input_bytes_encoded = input_string.encode()
-# print(type(input_bytes_encoded))
-# print(input_bytes_encoded)
-# output_string = input_bytes_encoded.decode()
-# print(type(output_string))
-# print(output_string)
-
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
 
-sock_service = socket.socket()
 
-sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
+def inviaComandi(socket):
+    while True:
+        try:
+            dati = input(
+                "Inserisci i dati dell'operazione (ko per terminare la connessione): ")
+        except EOFError:
+            print("\nOkay. Exit")
+            break
+        if not dati:
+            print("Non puoi inviare una stringa vuota!")
+            continue
+        if dati == 'ko':
+            print("Chiudo la connessione con il server!")
+            break
 
-print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
+        dati = dati.encode()
 
-while True:
-    try:
-        dati = input(
-            "Inserisci i dati dell'operazione (ko per terminare la connessione): ")
-    except EOFError:
-        print("\nOkay. Exit")
-        break
-    if not dati:
-        print("Non puoi inviare una stringa vuota!")
-        continue
-    if dati == 'ko':
-        print("Chiudo la connessione con il server!")
-        break
+        socket.send(dati)
 
-    dati = dati.encode()
+        dati = socket.recv(2048)
 
-    sock_service.send(dati)
+        if not dati:
+            print("Server non risponde. Exit")
+            break
 
-    dati = sock_service.recv(2048)
+        dati = dati.decode()
 
-    if not dati:
-        print("Server non risponde. Exit")
-        break
+        print("Ricevuto dal server:")
+        print(dati + '\n')
 
-    dati = dati.decode()
 
-    print("Ricevuto dal server:")
-    print(dati + '\n')
+def connessioneServer(address, port):
+    sock_service = socket.socket()
+    sock_service.connect((address, port))
+    print("Connesso a " + str((address, port)))
+    inviaComandi(sock_service)
+    sock_service.close()
 
-sock_service.close()
+
+if __name__ == "__main__":
+    connessioneServer(SERVER_ADDRESS, SERVER_PORT)
--- server.py
+++ server.py
@@ -1,70 +1,73 @@
 import socket
 
-
 SERVER_ADDRESS = '127.0.0.1'
 
 SERVER_PORT = 22224
 
-sock_listen = socket.socket()
 
-sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
-
-sock_listen.listen(5)
-
-print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT)))
-
-
-while True:
-    sock_service, addr_client = sock_listen.accept()
-    print("\nConnessione ricevuta da " + str(addr_client))
-    print("\nAspetto di ricevere i dati ")
-    contConn = 0
+def riceviComandi(socket):
     while True:
-        dati = sock_service.recv(2048)
-        contConn += 1
-        if not dati:
-            print("Fine dati dal client. Reset")
-            break
+        sock_service, addr_client = socket.accept()
+        print("\nConnessione ricevuta da " + str(addr_client))
+        print("\nAspetto di ricevere i dati ")
+        contConn = 0
+        while True:
+            dati = sock_service.recv(2048)
+            contConn += 1
+            if not dati:
+                print("Fine dati dal client. Reset")
+                break
 
-        dati = dati.decode()
-        print("Ricevuto: '%s'" % dati)
-        if dati == '0':
-            print("Chiudo la connessione con " + str(addr_client))
-            break
+            dati = dati.decode()
+            print("Ricevuto: '%s'" % dati)
+            if dati == '0':
+                print("Chiudo la connessione con " + str(addr_client))
+                break
 
-        dati = dati.split(";")  # trasforma da : piu;1;4 a [piu][1][4]
-        risposta = str()
+            dati = dati.split(";")  
+            risposta = str()
 
-        if dati[0] == "piu" or dati[0] == "meno" or dati[0] == "per" or dati[0] == "diviso":
-            try:
-                dati[1] = int(dati[1])
-                dati[2] = int(dati[2])
-            except ValueError:
-                print("ValueError")
-                risposta = "Non hai inserito i numeri correttamente."
+            if dati[0] == "piu" or dati[0] == "meno" or dati[0] == "per" or dati[0] == "diviso":
+                try:
+                    dati[1] = int(dati[1])
+                    dati[2] = int(dati[2])
+                except ValueError:
+                    print("ValueError")
+                    risposta = "Non hai inserito i numeri correttamente."
 
-            if risposta == "":  
-                risultato = int()
+                if risposta == "":  
+                    risultato = int()
 
-                if dati[0] == "piu":
-                    risultato = dati[1] + dati[2]
-                elif dati[0] == "meno":
-                    risultato = dati[1] - dati[2]
-                elif dati[0] == "per":
-                    risultato = dati[1] * dati[2]
-                else:
-                    risultato = dati[1] / dati[2]
+                    if dati[0] == "piu":
+                        risultato = dati[1] + dati[2]
+                    elif dati[0] == "meno":
+                        risultato = dati[1] - dati[2]
+                    elif dati[0] == "per":
+                        risultato = dati[1] * dati[2]
+                    else:
+                        risultato = dati[1] / dati[2]
 
-                risposta = "Il risultato dell'operazione " + \
-                    str(dati[0]) + " tra " + str(dati[1]) + " e " + \
-                    str(dati[2]) + " è uguale a " + str(risultato) + "."
-        else:
-            risposta = "Operazione non valida."
+                    risposta = "Il risultato dell'operazione " + \
+                        str(dati[0]) + " tra " + str(dati[1]) + " e " + \
+                        str(dati[2]) + " è uguale a " + str(risultato) + "."
+            else:
+                risposta = "Operazione non valida."
 
-        risposta = risposta.encode()
+            risposta = risposta.encode()
 
-        sock_service.send(risposta)
+            sock_service.send(risposta)
+        sock_service.close()
 
-    sock_service.close()
+
+def avviaServer(address, port):
+    sock_listen = socket.socket()
+    sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock_listen.bind((address, port))
+    sock_listen.listen(5)
+    print("Server in ascolto su %s." % str((address, port)))
+
+    riceviComandi(sock_listen)
+
+
+if __name__ == "__main__":
+    avviaServer(SERVER_ADDRESS, SERVER_PORT)
