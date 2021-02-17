538d04ee78d4b15b204ac6bc5f7bd767e5ca0280
pietro <pietro5801@gmail.com>
2021-02-17 13:38:29 +0100

    .

--- client.py
+++ client.py
@@ -1,13 +1,14 @@
-input_string = 'Hello'
-print(type(input_string))
-input_bytes_encoded = input_string.encode()
-print(type(input_bytes_encoded))
-print(input_bytes_encoded)
-output_string=input_bytes_encoded.decode()
-print(type(output_string))
-print(output_string)
-
 import socket
+
+# TEST SU COME FUNZIONA LA COMUNICAZIONE TRA CLIENT E SERVER
+# input_string = 'Hello'
+# print(type(input_string))
+# input_bytes_encoded = input_string.encode()
+# print(type(input_bytes_encoded))
+# print(input_bytes_encoded)
+# output_string = input_bytes_encoded.decode()
+# print(type(output_string))
+# print(output_string)
 
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
@@ -17,19 +18,21 @@ sock_service = socket.socket()
 sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
 
 print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
+
 while True:
     try:
-        dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ")
+        dati = input(
+            "Inserisci i dati dell'operazione (ko per terminare la connessione): ")
     except EOFError:
         print("\nOkay. Exit")
         break
     if not dati:
         print("Non puoi inviare una stringa vuota!")
         continue
-    if dati == '0':
+    if dati == 'ko':
         print("Chiudo la connessione con il server!")
         break
-    
+
     dati = dati.encode()
 
     sock_service.send(dati)
@@ -39,7 +42,7 @@ while True:
     if not dati:
         print("Server non risponde. Exit")
         break
-    
+
     dati = dati.decode()
 
     print("Ricevuto dal server:")
--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import socket
 
 
-
 SERVER_ADDRESS = '127.0.0.1'
 
 SERVER_PORT = 22224
@@ -21,23 +20,51 @@ while True:
     sock_service, addr_client = sock_listen.accept()
     print("\nConnessione ricevuta da " + str(addr_client))
     print("\nAspetto di ricevere i dati ")
-    contConn=0
+    contConn = 0
     while True:
         dati = sock_service.recv(2048)
-        contConn+=1
+        contConn += 1
         if not dati:
             print("Fine dati dal client. Reset")
             break
-        
+
         dati = dati.decode()
         print("Ricevuto: '%s'" % dati)
-        if dati=='0':
+        if dati == '0':
             print("Chiudo la connessione con " + str(addr_client))
             break
-        dati = "Risposta a : " + str(addr_client) + ". Il valore del contatore è : " + str(contConn)
 
-        dati = dati.encode()
+        dati = dati.split(";")  # trasforma da : piu;1;4 a [piu][1][4]
+        risposta = str()
 
-        sock_service.send(dati)
+        if dati[0] == "piu" or dati[0] == "meno" or dati[0] == "per" or dati[0] == "diviso":
+            try:
+                dati[1] = int(dati[1])
+                dati[2] = int(dati[2])
+            except ValueError:
+                print("ValueError")
+                risposta = "Non hai inserito i numeri correttamente."
+
+            if risposta == "":  
+                risultato = int()
+
+                if dati[0] == "piu":
+                    risultato = dati[1] + dati[2]
+                elif dati[0] == "meno":
+                    risultato = dati[1] - dati[2]
+                elif dati[0] == "per":
+                    risultato = dati[1] * dati[2]
+                else:
+                    risultato = dati[1] / dati[2]
+
+                risposta = "Il risultato dell'operazione " + \
+                    str(dati[0]) + " tra " + str(dati[1]) + " e " + \
+                    str(dati[2]) + " è uguale a " + str(risultato) + "."
+        else:
+            risposta = "Operazione non valida."
+
+        risposta = risposta.encode()
+
+        sock_service.send(risposta)
 
     sock_service.close()
